316f266400e0c7184bddb9d2a01902c7db0335c7
RaymondTianyiYu <32671694+RaymondTianyiYu@users.noreply.github.com>
2017-11-21 19:00:21 +0800

    more ser and add ()

--- comtest.py
+++ comtest.py
@@ -8,23 +8,27 @@ ser=serial.Serial(port='COM4')
 time.sleep(2)
 
 n=ser.write('1'.encode())
-n=ser.write('2')
-n=ser.write('3')
-n=ser.write('4')
+n=ser.write('2'.encode())
+n=ser.write('3'.encode())
+n=ser.write('4'.encode())
 
 
-print 'after write'
-print n
+print ('after write')
+print ('n')
 
 def run():
     action = "aaa"
     while action != "q":
-        print 'select which tone do you want to play ? 1,2 q and others for quit'
-        action = raw_input("> ")
+        print 'select which tone do you want to play ? 1,2,3,4,q and others for quit'
+        action = input("> ")
         if action == "1":
-            ser.write('1')
+            ser.write('1'.encode())
         elif action == "2":
-            ser.write('2')
+            ser.write('2'.encode())
+        elif action == "3":
+            ser.write('3'.encode())
+        elif action == "4":
+            ser.write('4'.encode)
         else :
             return
 
